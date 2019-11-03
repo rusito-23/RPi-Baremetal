b22794a46e778378a78106842e72c981604e3b08
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2019-11-03 19:31:15 -0300

    [Template] - First Commit -

---
+++ RPi-Baremetal/main.c
@@ -0,0 +1,64 @@
+
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+#include "hal/rpi-gpio.h"
+#include "hal/rpi-armtimer.h"
+#include "hal/rpi-interrupts.h"
+
+
+extern void __enable_interrupts ( void );
+
+//-------------------------------------------------------------------------
+void __attribute__((interrupt("IRQ"))) interrupt_vector ( void )
+{
+    static int lit = 0;
+
+    /* Clear the ARM Timer interrupt - it's the only interrupt we have
+       enabled, so we want don't have to work out which interrupt source
+       caused us to interrupt */
+    RPI_GetArmTimer()->IRQClear = 1;
+
+    //============================ CODE APP HERE =====================================
+    /* EXAMPLE COD: Flip the LED every 1 second */
+    if( lit )
+    {
+        LED_OFF();
+        lit = 0;
+    }
+    else
+    {
+        LED_ON();
+        lit = 1;
+    }
+}
+
+//-------------------------------------------------------------------------
+int notmain ( void )
+{
+    
+    RPI_GetIrqController()->Disable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
+
+    RPI_GetGpio()->LED_GPFSEL |= 1 << LED_GPFBIT;   // Configurar GPIO del LED como salida | Revisar definiciones en archivo rpi-gpio.h
+    
+    /* Setup the system timer interrupt */
+    /* Timer frequency = CLK frequency = 1HHz, CLK period = 1 useg */
+    RPI_GetArmTimer()->Load = 1000000; // 1.000.000 useg = 1 seg.
+
+    /* Setup the ARM Timer */
+    RPI_GetArmTimer()->Control =
+        RPI_ARMTIMER_CTRL_23BIT |
+        RPI_ARMTIMER_CTRL_ENABLE |
+        RPI_ARMTIMER_CTRL_INT_ENABLE |
+        RPI_ARMTIMER_CTRL_PRESCALE_1;
+
+
+    RPI_GetIrqController()->Enable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
+    __enable_interrupts();
+    
+    while(1) {
+    //============================ CODE APP HERE =====================================
+        
+    }
+    return(0);
+}
