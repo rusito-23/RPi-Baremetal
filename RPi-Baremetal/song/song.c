483d2a02065b128814eb9dacba16798e3c052936
Valerita Mollea <vale.mollea@gmail.com>
2019-11-12 09:59:09 -0300

    [QUIEN][SE][HA][TOMADO][TODO][EL][VINO]

--- RPi-Baremetal/song/song.c
+++ RPi-Baremetal/song/song.c
@@ -32,7 +32,7 @@ QUIEN SE HA TOMADO TODO EL VINO?
 */
 
 void createQuienSeHaTomado() {
-    PlayNote notes[24] = {
+    PlayNote notes[118] = {
 
         /* primera parte */
 
@@ -69,11 +69,140 @@ void createQuienSeHaTomado() {
         {.note= SECOND_OCTAVE(RE), .duration= QUAVER},
         {.note= SECOND_OCTAVE(MI), .duration= SEMIQUAVER},
 
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+
+        // bis
+
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(MI), .duration= QUAVER},
+
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(MI), .duration= QUAVER},
+
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(MI), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SI), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SOL), .duration= QUAVER},
+
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+
+        /* segunda parte */
+
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SI), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= QUAVER},
+
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SI), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= QUAVER},
+        {.note= SECOND_OCTAVE(MI), .duration= SEMIQUAVER},
+
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+
+        /* estrofa */
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        // bis
+
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        // segunda estrofa //
+
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(MI), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+
+        
+        // QUIEEEEEEEEEN
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= SECOND_OCTAVE(DO), .duration= CROTCHET},
+        {.note= SECOND_OCTAVE(MI), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(RE), .duration= SEMIQUAVER},
+
+        // se ha tomado todo el vino
+        {.note= FIRST_OCTAVE(SOL), .duration= FUSA},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(SOL), .duration= FUSA},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= FUSA},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= FUSA},
+        {.note= NO_NOTE, .duration= {.s = 0, .ds = 1}},
+        {.note= FIRST_OCTAVE(LA), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(SOL), .duration= SEMIQUAVER},
+        {.note= SECOND_OCTAVE(DO), .duration= SEMIQUAVER},
+        {.note= FIRST_OCTAVE(LA), .duration= QUAVER},
 
         // silencio final
         {.note= NO_NOTE, .duration= {.s = 2, .ds = 0}},
     };
 
-    Song song = {.notes= notes, .size= 24, .replay = 1};
+    Song song = {.notes= notes, .size= 118, .replay = 1};
     InitSong(song);
 }
